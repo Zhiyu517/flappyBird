606596ff578bdf7f5edbdc65e1e4f3721554c15a
use <use@11212111221.local>
2018-08-26 17:10:33 -0700

    all

---
+++ android/src/com/zhiyu/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.zhiyu.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.zhiyu.flappybird.flappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new flappyBird(), config);
+	}
+}
